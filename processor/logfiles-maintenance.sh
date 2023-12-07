c3a3bb794a9fc7d5d8884510850bd11fc4bb6194
Chris Dzombak <chris@dzombak.com>
2023-12-06 19:13:51 -0500

    combine log mgmt into a single job; run it under lock

--- processor/logfiles-maintenance.sh
+++ processor/logfiles-maintenance.sh
@@ -7,3 +7,6 @@ ionice -c2 -n7 nice -n 19 \
   find /ytdlbot-logs -mtime +1 -name "stats*.log*" -delete
 ionice -c2 -n7 nice -n 19 \
   find . -name "*.log*" -exec grep -q 'nothing to do' '{}' \; -delete
+
+ionice -c2 -n7 nice -n 19 \
+  chown abc:abc /ytdlbot-logs/*.log
